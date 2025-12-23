d0a76f7957849ee816e68cb55077b8b14e96596c
Xiermp <kotegavrilovi@gmail.com>
2025-12-23 00:51:09 -0800

    mmm i love css and html (no)

--- main/work_area.php
+++ main/work_area.php
@@ -3,8 +3,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>work_area</title>
+    <link rel="stylesheet" href="..\css\work_style.css">
 </head>
 <body>
     <a href="registration.php">reg</a>
+    <div class="main_er">
+        <div class="left_main_catalog">
+            <div class="icon_holder">
+                <div class="top_icons">a</div>
+                <div class="top_icons">s</div>
+                <div class="top_icons">fd</div>
+                <!-- <div class="top_icons">fd</div> -->
+            </div>
+
+            <div class="main_list">
+                <!-- <div class="documents"></div> -->
+                <select name="" id="" class="documents">
+                    <option value="">aa</option>
+                </select>
+                <div class="documents"></div>
+                <div class="documents"></div>
+                <div class="documents"></div>
+
+            </div>
+
+        </div>
+        <div class="writin_page">
+            <input type="text" name="" id="">
+        </div>
+    </div>
 </body>
 </html>
